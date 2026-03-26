6511acb2886e796ee3449933c967a3995c211a46
maram <maram@gmail.com>
2026-03-26 03:25:31 +0200

    second

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import express from "express";
 import bootstrap from "./src/App.controller.js";
 import dotenv from "dotenv";
-dotenv.config();
-
+// dotenv.config();
+dotenv.config({ path: process.env.NODE_ENV === 'production' ? false : '.env' });
 const app = express();
 bootstrap(app,express);
 const port = process.env.PORT||3000;
