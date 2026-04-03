604af67590a9853913a470f7a9a07b659c017ac9
maram <maram@gmail.com>
2026-04-03 15:57:50 +0200

    SS

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
-// import express from "express";
-// import bootstrap from "./src/App.controller.js";
-// import dotenv from "dotenv";
-// // dotenv.config();
-// dotenv.config({ path: process.env.NODE_ENV === 'production' ? false : '.env' });
-// const app = express();
-// bootstrap(app,express);
-// const port = process.env.PORT||3000;
-// app.listen(port, ()=>{
-//     console.log("server is running in port", port);
-// });
 import express from "express";
 import bootstrap from "./src/App.controller.js";
 import dotenv from "dotenv";
-
-if (process.env.NODE_ENV !== 'production') {
-  dotenv.config();
-}
-
+// dotenv.config();
+dotenv.config({ path: process.env.NODE_ENV === 'production' ? false : '.env' });
 const app = express();
-bootstrap(app, express);
-
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
+bootstrap(app,express);
+const port = process.env.PORT||3000;
+app.listen(port, ()=>{
     console.log("server is running in port", port);
 });
+// import express from "express";
+// import bootstrap from "./src/App.controller.js";
+// import dotenv from "dotenv";
+
+// if (process.env.NODE_ENV !== 'production') {
+//   dotenv.config();
+// }
+
+// const app = express();
+// bootstrap(app, express);
+
+// const port = process.env.PORT || 3000;
+// app.listen(port, () => {
+//     console.log("server is running in port", port);
+// });
--- src/utils/error/global-error.js
+++ src/utils/error/global-error.js
@@ -2,6 +2,7 @@ import fs from "fs";
 import path from "path";
 
 export const globalError = (error,req,res,next) =>{
+    
     if(req.file){
         const fullPath = path.resolve(req.file.path);
         fs.unlinkSync(fullPath);
