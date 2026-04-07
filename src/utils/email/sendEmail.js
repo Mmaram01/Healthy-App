545c0b7592d2cf3290712f795bbfb1f1ad81485a
maram <maram@gmail.com>
2026-04-07 11:09:53 +0200

    ba

--- src/utils/email/sendEmail.js
+++ src/utils/email/sendEmail.js
@@ -1,5 +1,5 @@
 // import { transporter } from "./transporter.js";
-
+// import { Resend } from "resend";
 // export const sendEmail = async ({to, subject, html}) =>{
 //   const info = await transporter.sendMail({
 //     from: `"Healthy App" <${process.env.EMAIL}>`,
@@ -15,13 +15,12 @@
 // };
 import dotenv from "dotenv";
 dotenv.config();
-import { Resend } from "resend";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+
 export const sendEmail = async ({ to, subject, html }) => {
   try {
-    await resend.emails.send({
-      from: "onboarding@resend.dev",
+    await transporter.sendMail({
+      from: `"Healthy App" <${process.env.EMAIL}>`,
       to,
       subject,
       html,
@@ -29,7 +28,7 @@ export const sendEmail = async ({ to, subject, html }) => {
 
     return true;
   } catch (err) {
-    console.log("Resend Error:", err.message);
+    console.log("Email Error:", err.message);
     return false;
   }
 };
--- src/utils/email/transporter.js
+++ src/utils/email/transporter.js
@@ -1,4 +1,4 @@
-
+import nodemailer from "nodemailer";
 // export const transporter = nodemailer.createTransport({
 //   host: "smtp.gmail.com",
 //   port:587,
@@ -22,20 +22,31 @@
 //   socketTimeout: 5000,
 // });
 
-import nodemailer from "nodemailer";
+
+
+// export const transporter = nodemailer.createTransport({
+//   host: "smtp.gmail.com",
+//   port: 465,            
+//   secure: true,         
+//   auth: {
+//     user: process.env.EMAIL,
+//     pass: process.env.PASS, 
+//   },
+
+//   connectionTimeout: 10000,
+//   greetingTimeout: 10000,
+//   socketTimeout: 15000,
+
+//   family: 4,
+// });
+
 
 export const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 465,            
-  secure: true,         
+  host: "smtp-relay.brevo.com",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL,
-    pass: process.env.PASS, 
+    pass: process.env.PASS,
   },
-
-  connectionTimeout: 10000,
-  greetingTimeout: 10000,
-  socketTimeout: 15000,
-
-  family: 4,
 });
