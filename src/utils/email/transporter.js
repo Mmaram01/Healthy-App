ebf5be3efd5fd212bc9e0dc70a836c6b0c638b2b
maram <maram@gmail.com>
2026-04-07 11:50:53 +0200

    tt

--- src/utils/email/transporter.js
+++ src/utils/email/transporter.js
@@ -43,8 +43,8 @@ import nodemailer from "nodemailer";
 
 export const transporter = nodemailer.createTransport({
   host: "smtp-relay.brevo.com",
-  port: 587,
-  secure: false,
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL,
     pass: process.env.PASS,
