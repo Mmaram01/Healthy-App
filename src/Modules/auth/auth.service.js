a2149cde0fdbb8fb22e92f7d0fd6890e7a442e0b
maram <maram@gmail.com>
2026-04-07 11:25:04 +0200

    sa

--- src/Modules/auth/auth.service.js
+++ src/Modules/auth/auth.service.js
@@ -28,7 +28,7 @@ export const signup = async(req, res, next)=>{
     type:"confirmEmail",
     expiresAt: new Date(Date.now() + 10 * 60 * 1000),
   });
-  sendOtpEmail({
+  await sendOtpEmail({
   to: user.email,
   otp: otpCode,
   type: "confirmEmail",
